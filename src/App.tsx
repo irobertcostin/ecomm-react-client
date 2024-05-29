b5d9452da80697c695e7ede53882eee62e3d719d
irobertcostin <irobertcostin@gmail.com>
2024-05-29 21:32:42 +0300

    other small UI updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/home-components/Home";
+import Footer from "./components/home-components/Footer";
+import ScrollToTop from "./components/home-components/ScrollToTop";
+import Navbar from "./components/home-components/Navbar";
+
 
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <ScrollToTop />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
